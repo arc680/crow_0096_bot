f90f490ff9146f3319817d21139d47fed0f39154
arc680 <maru.nslab@gmail.com>
2014-02-07 02:01:47 +0900

    Add template for tweet

---
+++ bot_tweet.rb
@@ -0,0 +1,53 @@
+class BotTweet
+    @@tweet = ""
+    def create_simple_tweet(cnt = 5)
+        tweet = "( っ'ω'c)"
+        keys = ["！", "？"]
+        cnt.times do
+            tweet = tweet + keys[rand(keys.length)]
+        end
+
+        return tweet
+    end
+
+    def create_nya_tweet(cnt = 5)
+        tweet = "( っ'ω'c)"
+        cnt.times do
+            tweet = tweet + "にゃ"
+        end
+
+        if rand(2) == 1
+            tweet = tweet + "ー"
+        end
+
+        return tweet
+    end
+
+    def create_tweet_from_template()
+        tweets = Array.new
+        open("./template") do |f|
+            f.each do |line|
+                tweets.push(line)
+            end
+        end
+
+        return tweets[rand(tweets.size)]
+    end
+
+    def create_tweet()
+        cnt = rand(10) + 1
+        pattern = rand(3)
+        if pattern == 1
+            @@tweet = create_simple_tweet(cnt)
+        elsif pattern == 2
+            @@tweet = create_nya_tweet(cnt)
+        else 
+            @@tweet = create_tweet_from_template() 
+        end
+    end
+
+    def get_tweet()
+        return @@tweet
+    end
+end
+
